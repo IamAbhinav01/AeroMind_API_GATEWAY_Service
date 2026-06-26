2a6e2cfe1eb403128c363ff9554a55fe9e0c3671
Abhinav <AbhinavSunil@hotmail.com>
2026-06-26 09:41:24 +0530

    Feat : Added dotenv configurations

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+func main(){
+
+
+	err:=godotenv.Load()
+
+	if err != nil{
+		log.Fatal("Error loading .env file")
+		fmt.Println("Unable to load .env")
+	}
+
+	PORT:=os.Getenv("PORT")
+
+}
