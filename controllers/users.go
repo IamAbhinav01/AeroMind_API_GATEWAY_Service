16ac7afe73cfe021b540151f34a1bdc5ff7911df
Abhinav <AbhinavSunil@hotmail.com>
2026-06-29 11:03:46 +0530

    Feat : added controller logic

---
+++ controllers/users.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"AeromindGO/services"
+	"net/http"
+)
+
+type UserController struct {
+	UserService services.UserService
+}
+
+func (user *UserController) Create(w http.ResponseWriter , r *http.Request){
+	user.UserService.Create()
+	w.Write([] byte("user fetching ongoing ..."))
+}
+
+func NewUserController(_userService services.UserService) *UserController{
+	return &UserController{
+		UserService: _userService,
+	}
+} 
