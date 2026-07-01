85d49a9252f18f8f163440dc325bfb5e801218a8
Abhinav <AbhinavSunil@hotmail.com>
2026-07-01 11:13:25 +0530

    Feat : added the formatters but issue is how to store it as a json , need to work on marshalling and encoding

--- utils/responseFormatters/errorResponse.go
+++ utils/responseFormatters/errorResponse.go
@@ -1,1 +1,20 @@
 package utils
+
+import "net/http"
+
+func successResponse(w http.ResponseWriter,status int,message string,data any){
+	response := map[string]any{
+		response["status"] = status
+		response["message"] = message
+		response["data"] = data
+		
+	}
+}
+func errorResponse(w http.ResponseWriter,status int,message string,err error){
+	response := map[string]any{
+		response["status"] = status
+		response["message"] = message
+		response["error"] = err
+
+	}
+}
