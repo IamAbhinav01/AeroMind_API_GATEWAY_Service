2b130302a831af8ef37c8ab40af9127b860dd49d
Abhinav <AbhinavSunil@hotmail.com>
2026-07-01 11:45:26 +0530

    Feat : Added the response formatters for all handler endpoints

--- controllers/users.go
+++ controllers/users.go
@@ -20,18 +20,30 @@ func (user *UserController) Create(w http.ResponseWriter , r *http.Request){
 }
 
 func (user *UserController) GetUserByID(w http.ResponseWriter,r *http.Request){
-	user.UserService.GetUserByID(1)
-	w.Write([] byte("user fetched for the id 2 .."))
+	response,err:=user.UserService.GetUserByID(2)
+	if err != nil{
+		utils.ErrorResponse(w,http.StatusAccepted,"Error occured while fetching user.",err)
+	} else {
+		utils.SuccessResponse(w,http.StatusCreated,"User fetched successfully.",response)
+	}
 }
 
 func (user *UserController) GetAllUsers(w http.ResponseWriter,r *http.Request){
-	user.UserService.GetAllUsers()
-	w.Write([] byte("all users fetched .."))
+	response,err:=user.UserService.GetAllUsers()
+	if err != nil{
+		utils.ErrorResponse(w,http.StatusAccepted,"Error occured while fetching users.",err)
+	} else {
+		utils.SuccessResponse(w,http.StatusCreated,"Users fetched successfully.",response)
+	}
 }
 
 func (user *UserController) DeleteUserByID(w http.ResponseWriter,r *http.Request){
-	user.UserService.DeleteUserByID(1)
-	w.Write([] byte("user deleted for the id 1 .."))
+	response,err:=user.UserService.DeleteUserByID(2)
+	if err != nil{
+		utils.ErrorResponse(w,http.StatusAccepted,"Error occured while deleting user.",err)
+	} else {
+		utils.SuccessResponse(w,http.StatusCreated,"User deleted successfully.",response)
+	}
 }
 
 func NewUserController(_userService services.UserService) *UserController{
--- services/users.go
+++ services/users.go
@@ -2,15 +2,17 @@ package services
 
 import (
 	DB "AeromindGO/DB/repositories"
+	"AeromindGO/models"
+	"database/sql"
 	"fmt"
 	"log"
 )
 
 type UserService interface {
 	Create() (int, error)
-	GetUserByID(id int)
-	GetAllUsers() 
-	DeleteUserByID(id int)
+	GetUserByID(id int)(models.User, error)
+	GetAllUsers() ([]models.User, error)
+	DeleteUserByID(id int)(sql.Result, error)
 }
 
 type UserServiceImpl struct {
@@ -26,32 +28,35 @@ func (user *UserServiceImpl) Create() (int, error) {
 	return response,nil;
 }
 
-func (user *UserServiceImpl) GetUserByID(id int) {
+func (user *UserServiceImpl) GetUserByID(id int) (models.User, error) {
 	fmt.Println("Fetching user details based on id")
 	response,err:=user.UserRepository.GetUserByID(id)
 	if err != nil{
 		log.Fatal("Error while fetching the user by id : ",err)
 	}
-	fmt.Println("User details fetched successfully : ",response)
+	fmt.Println("User details fetched successfully ")
+	return response,nil
 }
 
-func (user *UserServiceImpl) GetAllUsers(){
+func (user *UserServiceImpl) GetAllUsers()([]models.User, error){
 	fmt.Println("Fetching all users")
 	response,err:=user.UserRepository.GetAllUsers()
 	if err != nil{
 		log.Fatal("Error while fetching all users : ",err)
 	}
-	fmt.Println("All users fetched successfully : ",response)
+	fmt.Println("All users fetched successfully  ")
+	return response,nil
 }
 
-func (user *UserServiceImpl) DeleteUserByID(id int){
+func (user *UserServiceImpl) DeleteUserByID(id int)(sql.Result, error){
 
 	fmt.Println("Deleting user based on id")
 	response,err := user.UserRepository.DeleteUserByID(id)
 	if err != nil{
 		log.Fatal("Error while deleting the user : ",err)
 	}
-	fmt.Println("User deleted successfully : ",response)
+	fmt.Println("User deleted successfully ")
+	return response,nil
 }
 
 func NewUserService(_userRepository DB.UserRepository) UserService{
