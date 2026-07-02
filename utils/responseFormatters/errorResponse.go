a1215b25d9b06be424d32f8832c7aaec176679b0
Abhinav <AbhinavSunil@hotmail.com>
2026-07-02 10:37:50 +0530

    Feat : refactore JSON marshalling

--- utils/responseFormatters/errorResponse.go
+++ utils/responseFormatters/errorResponse.go
@@ -1,36 +1,12 @@
 package utils
 
 import (
-	"encoding/json"
+	utils "AeromindGO/utils/json"
 	"net/http"
 )
 
 
 
-
-
-
-
-func toJSON(w http.ResponseWriter,status int,data any)error{
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-
-	return json.NewEncoder(w).Encode(data)
-
-}
-
-
-
-
-
-func SuccessResponse(w http.ResponseWriter,status int,message string,data any) error{
-	response := map[string]any{}
-	response["status"] = status
-	response["message"] = message
-	response["data"] = data
-	return toJSON(w,status,response)
-}
 func ErrorResponse(w http.ResponseWriter,status int,message string,err error) error{
 	response := map[string]any{}
 	response["status"] = status
@@ -40,5 +16,5 @@ func ErrorResponse(w http.ResponseWriter,status int,message string,err error) er
 	} else {
 		response["error"] = nil
 	}
-	return toJSON(w,status,response)
+	return utils.ToJSON(w, status, response)
 }
--- utils/responseFormatters/successResponse.go
+++ utils/responseFormatters/successResponse.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	utils "AeromindGO/utils/json"
+	"net/http"
+)
+
+func SuccessResponse(w http.ResponseWriter, status int, message string, data any) error {
+	response := map[string]any{}
+	response["status"] = status
+	response["message"] = message
+	response["data"] = data
+	return utils.ToJSON(w, status, response)
+}
