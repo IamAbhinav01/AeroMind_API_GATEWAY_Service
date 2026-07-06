880c52612efef03e430ed4b6acc1c0d7e577ac1f
Abhinav <AbhinavSunil@hotmail.com>
2026-07-06 10:47:34 +0530

    Feat : added logic for getting bucket status and as well as resting the bucket

--- utils/limiter/tokenBucketService.go
+++ utils/limiter/tokenBucketService.go
@@ -2,7 +2,10 @@ package limiter
 
 import (
 	config "AeromindGO/config/env"
+	"context"
 	"fmt"
+	"math"
+	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -10,7 +13,8 @@ import (
 
 type RedisBucketService interface {
 	GetClientKey(identifier string) string
-	getBucketStatus(identifier string) 
+	resetBucket(identifier string) error
+	getBucketStatus(identifier string) (*BucketStatus,error)
 }
 
 type RedisBucketServiceImpl struct {
@@ -22,7 +26,15 @@ type RedisBucketServiceImpl struct {
 }
 
 type BucketStatus struct{
-	exists bool `json:"exists"`
+	Exists          bool   `json:"exists"`
+	Identifier      string `json:"identifier"`
+	StoredTokens    int    `json:"storedTokens"`
+	ProjectedTokens int    `json:"projectedTokens"`
+	Capacity        int    `json:"capacity"`
+	LastRefillTime  int64  `json:"lastRefillTime"`
+	TTLMs           int64  `json:"ttlMs"`
+	RedisKey        string `json:"redisKey"`
+	Error           string `json:"error,omitempty"`
 }
 
 func (bucket *RedisBucketServiceImpl) GetClientKey(identifier string) string{
@@ -33,13 +45,66 @@ func (bucket *RedisBucketServiceImpl) GetClientKey(identifier string) string{
 
 }
 
-func (bucket *RedisBucketServiceImpl) getBucketStatus(identifier string){
+func(bucket *RedisBucketServiceImpl) resetBucket(identifier string) error {
 
+	ctx:=context.Background()
+	key:=bucket.GetClientKey(identifier)
+	return bucket.redisClient.Del(ctx,key).Err()
 
+}
+
+func (bucket *RedisBucketServiceImpl) getBucketStatus(identifier string) (*BucketStatus,error){
+
+	ctx:=context.Background()
 	key:=bucket.GetClientKey(identifier)
 
-	
 
+	data,err:=bucket.redisClient.HMGet(ctx,key,"tokens",
+		"lastRefillTime",).Result()
+
+
+	if err != nil{
+		return &BucketStatus{
+			Exists: false,
+			Identifier: identifier,
+			Error: err.Error(),
+		},nil
+	}
+
+	ttl,TTLerr:=bucket.redisClient.PTTL(ctx,key).Result()
+
+	if TTLerr != nil{
+		return nil,TTLerr
+	}
+
+	if data[0] == nil {
+		return &BucketStatus{
+			Exists: false,
+			Identifier: identifier,
+		},nil
+	}
+
+	tokens,_:=strconv.ParseFloat(data[0].(string),64)
+	lastFill,_:=strconv.ParseInt(data[1].(string),10,64)
+
+	refillRate:= float64(bucket.defaultRefill)/float64(bucket.defaultTimeout)
+	elapsed:= time.Now().UnixMilli() - lastFill
+
+
+	projected:=math.Min(
+		float64(bucket.defaultCapacity),
+		tokens+float64(elapsed)*refillRate,
+	)
+	return &BucketStatus{
+		Exists        :  true,
+		Identifier      :identifier,
+		StoredTokens    :int(tokens),
+		ProjectedTokens :int(projected),
+		Capacity        :bucket.defaultCapacity,
+		LastRefillTime  :lastFill,
+		TTLMs           :ttl.Milliseconds(),
+		RedisKey        :key,
+	},nil
 
 }
 
