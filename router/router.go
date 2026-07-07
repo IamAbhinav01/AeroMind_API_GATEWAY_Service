5740348253caa7b2c1148f6a82c5c8df43ea53dc
Abhinav <AbhinavSunil@hotmail.com>
2026-07-07 10:51:51 +0530

    Feat : added cors for request response transfer

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 type Router interface {
@@ -12,6 +13,16 @@ type Router interface {
 func SetupRouter(UserRouter Router) *chi.Mux {
 
 	chiRouter := chi.NewRouter()
+	
+	// Basic CORS
+	chiRouter.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "x-access-token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	// Mount application routes under /api/v1 so external clients
 	// can call paths like /api/v1/booking/... and match proxy prefixes.
