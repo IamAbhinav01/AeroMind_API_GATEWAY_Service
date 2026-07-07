86c8128ca78b97c171905a41da253c8115d319b0
Abhinav <AbhinavSunil@hotmail.com>
2026-07-07 09:58:18 +0530

    Feat : Implemeted create reverse-proxy and now needed to create the target url and their prefix

---
+++ utils/reverse-proxy/reverse_proxy.go
@@ -0,0 +1,39 @@
+package reverseproxy
+
+import (
+	"fmt"
+	"log"
+
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func createProxyMiddleware(targetURL string, pathPrefix string) http.Handler{
+	target,err:=url.Parse(targetURL)
+
+	if err != nil{
+		fmt.Println("Error while parsing the url: ",err)
+		return nil
+	}
+
+	 proxy := &httputil.ReverseProxy{
+        Rewrite: func(req *httputil.ProxyRequest) {
+            // Set the target host and scheme
+            req.SetURL(target)
+
+			req.Out.Host = target.Host
+
+			req.Out.URL.Path = pathPrefix + req.In.URL.Path
+
+			log.Printf("%s -> %s%s",req.In.RemoteAddr,target.Host,req.Out.URL.Path)
+            
+        },
+		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
+			http.Error(w,err.Error(),http.StatusBadGateway)
+		},
+    }
+	return proxy
+
+
+}
